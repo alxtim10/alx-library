2e4befa2c1d561de3ea15b2ffcb7bcfc4c0bf94e
alex <alxtim10@gmail.com>
2024-05-18 21:55:13 +0700

    update final

---
+++ bookshelf_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO public."Bookshelf"(
+	"BOOKSHELF_ID", "NAME")
+	VALUES (1, 'COMIC');
+
+INSERT INTO public."Bookshelf"(
+	"BOOKSHELF_ID", "NAME")
+	VALUES (2, 'EDUCATION');
+
+INSERT INTO public."Bookshelf"(
+	"BOOKSHELF_ID", "NAME")
+	VALUES (3, 'HISTORY');
